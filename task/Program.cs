ca477ba24c6264b55c820a8dadc9e52d11b6c2cd
Nikita <vinidiktovn@gmail.com>
2023-01-16 13:32:57 +0300

    final stage

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,5 @@
 ﻿string[] strings = { "hello", "2", "world", ":-)",};
-var result = strings.Where(s => s.Length <= 3).ToArray();
+    var result = strings.Where(s => s.Length <= 3).ToArray();
+        Console.WriteLine(string.Join(", ", result)); 
+        Console.ReadLine();
+            return(0);
